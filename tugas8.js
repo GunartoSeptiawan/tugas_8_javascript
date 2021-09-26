470cc3067954f0a26126ac0cae50e3da430cee67
Gunarto Septiawan <gunartosept@gmail.com>
2021-09-26 08:31:10 +0700

    tugas_8_javscript
    Tugas Niomic

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function panggilObjects(){
+  var rumah = {
+    type: 'dualantai',
+    harga: 700000000,
+    warna: 'putih',
+  }
+  rumah.harga = 900000000;
+  console.log(rumah)
+  console.log(rumah.harga)
+}
+panggilObjects();
